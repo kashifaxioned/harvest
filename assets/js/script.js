779e1a2723fd4290570950c18d611fe942d6071a
kashifaxioned <kashifa@axioned.com>
2022-04-29 12:03:14 +0530

    hamburger fucntionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,22 @@
 
 */
 
+// haburger functionality
 
+$(".hamburger-btn").click(showNav)
+$(".close-btn").click(hideNav)
+
+let navSection = $(".nav-section")
+
+function showNav() {
+  $(".hamburger-btn").addClass("hide")
+  navSection.addClass("responsive-nav")
+}
+
+function hideNav() {
+  $(".hamburger-btn").removeClass("hide")
+  navSection.removeClass("responsive-nav")
+}
 
 
 
