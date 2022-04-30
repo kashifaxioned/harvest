a826832a61ed1c445aca604436ad69ab35bf975a
kashifaxioned <kashifa@axioned.com>
2022-04-30 08:36:16 +0530

    slick slider added for portfolio function

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,40 @@ $(".banner-list").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 })
+
+// slick slider functionality for work section
+
+$(".portfolio").slick({
+  autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
+
